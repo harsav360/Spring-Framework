9bb45ebd43898d24f806a85abae8df7e512868e1
harsav360 <annu7418@gmail.com>
2023-06-19 17:26:21 +0530

    Tight Coupling Cont..

--- src/main/java/com/harsav360/spring/basics/springin5steps/BinarySearchImpl.java
+++ src/main/java/com/harsav360/spring/basics/springin5steps/BinarySearchImpl.java
@@ -4,7 +4,9 @@ public class BinarySearchImpl {
 	
 	public int binarySearch(int[] numbers,int number)
 	{
-		//
+		BubbleSortAlgorithm  bSort = new BubbleSortAlgorithm();
+		int[] result = bSort.sort(numbers);
+		System.out.println(result);
 		return 3;
 	}
 
